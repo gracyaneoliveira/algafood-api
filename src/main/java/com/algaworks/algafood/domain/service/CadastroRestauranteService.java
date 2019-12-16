214a4982f9a57de0cadb13b5963dce073088096b
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2019-12-15 22:21:52 -0300

    4.31. Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -29,6 +29,19 @@ public class CadastroRestauranteService {
 		}
 
 		restaurante.setCozinha(cozinha);
+
+		return restauranteRepository.salvar(restaurante);
+	}
+
+	public Restaurante atualizar(Long restauranteId, Restaurante restaurante) {
+		
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com o código %d", cozinhaId));
+		}
 		
 		return restauranteRepository.salvar(restaurante);
 	}
