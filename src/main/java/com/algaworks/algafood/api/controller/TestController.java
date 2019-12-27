9c9fcc4bbed35c8795a169688734fbbd8490f8e9
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2019-12-26 22:37:19 -0300

    5.6. Criando consultas com query methods

---
+++ src/main/java/com/algaworks/algafood/api/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/teste")
+public class TestController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(String nome){
+		return cozinhaRepository.findTodasByNome(nome);
+	}
+
+	@GetMapping("/cozinhas/unica-por-nome")
+	public Optional<Cozinha> cozinhaPorNome(String nome){
+		return cozinhaRepository.findByNome(nome);
+	}
+}
