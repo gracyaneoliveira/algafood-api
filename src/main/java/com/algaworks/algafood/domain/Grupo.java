15164c4763df8b9c3f3b8d886aee54e00fb0424c
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2019-12-29 15:43:00 -0300

    6.9. Desafio: mapeando relacionamentos muitos-para-muitos

---
+++ src/main/java/com/algaworks/algafood/domain/Grupo.java
@@ -0,0 +1,39 @@
+package com.algaworks.algafood.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import com.algaworks.algafood.domain.model.Permissao;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Grupo {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", 
+		joinColumns = @JoinColumn(name = "grupo_id"),
+		inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes = new ArrayList<>();
+	
+}
