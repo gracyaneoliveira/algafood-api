312334d990c8e897f1eb4db30e31840adcff195a
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-09-08 23:44:14 -0300

    9.14. Usando o Resource Bundle do Spring como Resource Bundle do Bean Validation

---
+++ src/main/java/com/algaworks/algafood/core/validation/ValidationConfig.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.core.validation;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidationConfig {
+
+	/*
+	 * Indica que o message default da aplicacao sera o messages.properies do Spring
+	 * e nao ValidationMessages.properties do JPA
+	 * */
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+}
