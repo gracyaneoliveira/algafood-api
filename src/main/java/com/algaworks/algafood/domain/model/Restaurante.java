e5dc34a7621e6660746ee86a836bc3f086566812
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-09-09 01:38:08 -0300

    9.17. Criando constraints de validação customizadas em nível de classe

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -29,11 +29,13 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.core.validation.Multiplo;
 import com.algaworks.algafood.core.validation.TaxaFrete;
+import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@ValorZeroIncluiDescricao(valorField = "taxaFrete", descricaoField = "nome", descricaoObrigatoria="Frete Grátis")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -49,9 +51,9 @@ public class Restaurante {
 	private String nome;
 
 //	@DecimalMin("1")
-//	@PositiveOrZero //(message = "{TaxaFrete.invalida}")
 //	@TaxaFrete
-	@Multiplo(numero = 5)
+//	@Multiplo(numero = 5)
+	@PositiveOrZero //(message = "{TaxaFrete.invalida}")
 	@NotNull
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
