52f6285819e0fffb9a5853d23490f9006a880ea0
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-09-25 23:52:35 -0300

    12.24. Refatorando o código de regras para transição de status de pedidos

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -21,6 +21,8 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -83,5 +85,31 @@ public class Pedido {
 
 	public void atribuirPedidoAosItens() {
 	    getItens().forEach(item -> item.setPedido(this));
+	}
+	
+	public void confirmar() {
+		setStatus(StatusPedido.CONFIRMADO);
+		setDataConfirmacao(OffsetDateTime.now());
+	}
+	
+	public void entregar() {
+		setStatus(StatusPedido.ENTREGUE);
+		setDataEntrega(OffsetDateTime.now());
+	}
+	
+	public void cancelar() {
+		setStatus(StatusPedido.CANCELADO);
+		setDataCancelamento(OffsetDateTime.now());
+	}
+	
+	private void setStatus(StatusPedido novostatus) {
+		if(getStatus().naoPodeAlterarPara(novostatus)) {
+			throw new NegocioException(
+	                String.format("Status do pedido %d não pode ser alterado de %s para %s",
+	                        getId(), getStatus().getDescricao(), 
+	                        novostatus.getDescricao()));
+		}
+		
+		this.status = novostatus;
 	}
 }
