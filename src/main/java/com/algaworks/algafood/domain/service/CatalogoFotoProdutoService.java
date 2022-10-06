a73004216bc84bd260a2c0915ac827d943c4f315
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-10-05 23:20:51 -0300

    14.9. Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.service;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import com.algaworks.algafood.domain.service.FotoStorageService.NovaFoto;
 
 // https://app.algaworks.com/forum/topicos/84365/duvida-quando-o-arquivo-vai-sem-extensao
 
@@ -16,12 +18,16 @@ import com.algaworks.algafood.domain.repository.ProdutoRepository;
 public class CatalogoFotoProdutoService {
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;	
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private FotoStorageService fotoStorage;
 	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
 		Long restauranteId = foto.getRestauranteId();
 		Long produtoId = foto.getProduto().getId();
+		String novoNomeArquivo = fotoStorage.gerarNomeArquivo(foto.getNomeArquivo());
 		
 		Optional<FotoProduto> fotoExistente = produtoRepository
 				.findFotoById(restauranteId, produtoId);
@@ -30,7 +36,18 @@ public class CatalogoFotoProdutoService {
 			produtoRepository.delete(fotoExistente.get());
 		}
 		
-		return produtoRepository.save(foto);
+		foto.setNomeArquivo(novoNomeArquivo);
+		foto = produtoRepository.save(foto);
+		produtoRepository.flush();
+		
+		NovaFoto novaFoto = NovaFoto.builder()
+				.nomeArquivo(foto.getNomeArquivo())
+				.inputStream(dadosArquivo)
+				.build();
+		
+		fotoStorage.armazenar(novaFoto);
+		
+		return foto;		
 	}
 
 }
